dcaea1793751e2cc5757b106dad119e708a96db1
Palak-M <77509368+Palak-M@users.noreply.github.com>
2022-02-09 14:16:10 -0800

    Basic-JS_Game.JS (#2)
    * Basic-JS_Game.JS
    
    * Adding gitignore
    
    Co-authored-by: Palak Mattoo <palak@Palaks-MacBook-Pro.local>

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -6,5 +6,19 @@ function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+  let word1 = word.toLowerCase();
+  let guess1 = guess.toLowerCase();
+
+  let count = 0;
+  const chars = guess1.split("");
+  for(let s of word1){
+      let str = (ch) => ch === s;
+      let index = chars.findIndex(str);
+      if(index >= 0){
+        count++;
+        chars.splice(index,1);
+      }
+  }
+
+  return count; 
 }
