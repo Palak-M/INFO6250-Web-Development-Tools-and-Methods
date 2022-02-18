6ed7e689e8bccf820bf5d352078d6dc75dc10079
Palak-M <77509368+Palak-M@users.noreply.github.com>
2022-02-18 15:35:28 -0800

    Basic express (#3)
    * .gitignore
    
    * Basic-Express Assignment
    
    Co-authored-by: Palak Mattoo <palak@Palaks-MacBook-Pro.local>

--- work/basic-express/chat-web.js
+++ work/basic-express/chat-web.js
@@ -1,14 +1,20 @@
 const chatWeb = {
   chatPage: function(chat) {
-    // Fill in anything below!
     return `
       <!doctype html>
       <html>
         <head>
+
+        <link rel="stylesheet" href="/chat.css">
           <title>Chat</title>
         </head>
         <body>
           <div id="chat-app">
+          <div class="heading">
+          <div class="profile">
+          <h1>INFO6250-CHATROOM</h1>
+          </div>
+          </div>
             <div class="display-panel">
               ${chatWeb.getUserList(chat)}
               ${chatWeb.getMessageList(chat)}
@@ -21,23 +27,44 @@ const chatWeb = {
   },
 
   getMessageList: function(chat) {
-    return `<ol class="messages">` +
-      // Fill in!
-      `</ol>`;
+    return (`<ol class="messages">` +
+      chat.messages.map(
+        (message) =>`
+        <li>
+          <div class="message">
+            <div class="sender-info">
+              <span class="sendername">${message.sender}</span>
+              <span class="sendtime">${message.timestamp}</span> 
+            </div>
+            <p>${message.text}</p>
+          </div>
+        </li>
+        `
+      )
+      .join("") +
+      `</ol>`);
   },
   getUserList: function(chat) {
     return `<ul class="users">` +
-    Object.values(chat.users).map( user => `
+    Object.values(chat.users).map( (user) => `
       <li>
         <div class="user">
-          <span class="username">${user}</span>
+          <span class="sendername">${user}</span>
         </div>
       </li>
-    `).join('') +
+    `).join("") +
     `</ul>`;
   },
   getOutgoing: function() {
-    // Fill in!
-  }
+    return `
+    <div class="addmessage">
+    <form action="/chat" method="POST">
+    <input class="sendername" name="username" value=Amit type="hidden"/>
+    <input class="messagetosend" name="text" value="" placeholder="Type a message!!">
+    <button class="sendbutton" type="submit">Send</button>
+    </form>
+    </div>
+    `;
+  },
 };
 module.exports = chatWeb;
--- work/basic-express/chat.js
+++ work/basic-express/chat.js
@@ -6,16 +6,18 @@ const users = {
 const messages = [
   {
     sender: "Amit",
+    timestamp: new Date("2022-02-02  15:30:00"),
     text: "You up?",
   },
   {
     sender: "Bao",
+    timestamp: new Date("2022-02-02  15:50:00"),
     text: "Yeah, still working on this INFO6250 work, but I keep getting distracted by cat videos",
   }
 ];
 
-function addMessage({ sender, text }) {
-  // Fill in!
+function addMessage({ sender, timestamp, text }) {
+  messages.push({sender,timestamp, text})
 }
 
 const chat = {
